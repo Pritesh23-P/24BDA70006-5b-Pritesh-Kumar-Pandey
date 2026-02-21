7397cad004191cd6970f296e47847f41b26c30e5
Pritesh23-P <pritesh0525@gmail.com>
2026-02-22 00:58:39 +0530

    Prepared project for Vercel deployment

--- app.js
+++ app.js
@@ -12,10 +12,14 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
+    .catch(err => console.error("MongoDB Error:", err));
 
+// Routes
 app.use('/', require('./routes/studentRoutes'));
 
 // Error Handler
@@ -23,6 +27,5 @@ app.use((err, req, res, next) => {
     res.status(500).render('error', { error: err.message });
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-});
+// IMPORTANT: Export app instead of listen
+module.exports = app;
